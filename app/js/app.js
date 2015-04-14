eedbb471fa72ba3223a3a1ae9e32152ab4b614f6
KletusVandamme <matt.hill.email@gmail.com>
2015-04-14 02:03:53 -0700

    moved some input array init to service

--- app/js/app.js
+++ app/js/app.js
@@ -7,12 +7,9 @@ angular.module('myApp', ['testService', 'subModule'])
   this.inCurr = 'EUR';
   this.currencies = currencyConverter.currencies;
 
-  this.input1 = 10;
-  this.input2 = 20;
-  this.input3 = 30;
-  this.input4 = 40;
   this.results = monthlyResults;
   this.inputsArray = [];
+  this.inputsHash = {};
 
   this.getResults = function getResults(){
     var inputsHash = {
@@ -26,41 +23,49 @@ angular.module('myApp', ['testService', 'subModule'])
   };
 
 
-  this.initializeInputsDisplays = function initializeInputsDisplays(){
+  // this.initializeInputsDisplays = function initializeInputsDisplays(){
+  //   var i1 = new Input();
+  //   i1.name = 'projectCost';
+  //   i1.displayName = 'Project Cost ($)';
+  //   i1.value = 1400000;
+  //   i1.valueString = 'xxx';
+  //   i1.min = 0;
+  //   i1.max = 2000000;
+  //   i1.step = 100;
+  //
+  //   var i2 = new Input();
+  //   i2.name = 'ownerEquity';
+  //   i2.displayName = 'Owner Equity (%)';
+  //   i2.value = 25;
+  //   i2.valueString = 'zzz';
+  //   i2.min = 0;
+  //   i2.max = 100;
+  //   i2.step = 1;
+  //
+  //   var i3 = new Input();
+  //   i3.name = 'interestRate';
+  //   i3.displayName = 'Interest Rate (WACC) (%)';
+  //   i3.value = 12;
+  //   i3.valueString = 'xxx';
+  //   i3.min = 0;
+  //   i3.max = 30;
+  //   i3.step = 1;
+  //
+  //   this.inputsArray.push(i1);
+  //   this.inputsArray.push(i2);
+  //   this.inputsArray.push(i3);
+  // }
 
-    var i1 = new Input();
-    i1.name = 'projectCost';
-    i1.displayName = 'Project Cost ($)';
-    i1.value = 1400 * 1000;
-    i1.valueString = '1,400,000';
-    i1.min = 0;
-    i1.max = 2000 * 1000;
-    i1.step = 1;
+  this.createInputsHash = function() {
+    for(var i = 0; i < this.inputsArray.length; i++){
+      var inputObject = this.inputsArray[i];
+      this.inputsHash[inputObject.name] = inputObject.value;
+    }
 
-    var i2 = new Input();
-    i2.name = 'ownerEquity';
-    i2.displayName = 'Owner Equity (%)';
-    i2.value = 25;
-    i2.valueString = 'zzz';
-    i2.min = 0;
-    i2.max = 100;
-    i2.step = 1;
+  };
 
-    var i3 = new Input();
-    i3.name = 'interestRate';
-    i3.displayName = 'Interest Rate (WACC) (%)';
-    i3.value = 12;
-    i3.valueString = 'xxx';
-    i3.min = 0;
-    i3.max = 30;
-    i3.step = 1;
-
-
-    this.inputsArray.push(i1);
-    this.inputsArray.push(i2);
-    this.inputsArray.push(i3);
-  }
-
-  this.initializeInputsDisplays();
-
+  this.inputsArray = currencyConverter.initializeInputsDisplays();
+  // this.initializeInputsDisplays();
+  this.createInputsHash();
+  console.log(this.inputsHash);
 }]);
--- app/js/subModule.js
+++ app/js/subModule.js
@@ -1,14 +1,18 @@
 angular.module('subModule', [])
 .factory('currencyConverter', function() {
-  var currencies = ['USD', 'EUR', 'CNY'];
-  var usdToForeignRates = {
-    USD: 1,
-    EUR: 0.74,
-    CNY: 6.09
-  };
-  var convert = function (amount, inCurr, outCurr) {
-    return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
-  };
+
+
+  var inputsArray = [];
+
+  // var currencies = ['USD', 'EUR', 'CNY'];
+  // var usdToForeignRates = {
+  //   USD: 1,
+  //   EUR: 0.74,
+  //   CNY: 6.09
+  // };
+  // var convert = function (amount, inCurr, outCurr) {
+  //   return amount * usdToForeignRates[outCurr] / usdToForeignRates[inCurr];
+  // };
 
   var calcAnnualIncome = function (inputsHash){
     var annualIncome = inputsHash['input1'] +
@@ -19,7 +23,40 @@ angular.module('subModule', [])
     return annualIncome;
   };
 
+  this.initializeInputsDisplays = function initializeInputsDisplays(){
+    var i1 = new Input();
+    i1.name = 'projectCost';
+    i1.displayName = 'Project Cost ($)';
+    i1.value = 1400000;
+    i1.valueString = 'xxx';
+    i1.min = 0;
+    i1.max = 2000000;
+    i1.step = 100;
 
+    var i2 = new Input();
+    i2.name = 'ownerEquity';
+    i2.displayName = 'Owner Equity (%)';
+    i2.value = 25;
+    i2.valueString = 'zzz';
+    i2.min = 0;
+    i2.max = 100;
+    i2.step = 1;
+
+    var i3 = new Input();
+    i3.name = 'interestRate';
+    i3.displayName = 'Interest Rate (WACC) (%)';
+    i3.value = 12;
+    i3.valueString = 'xxx';
+    i3.min = 0;
+    i3.max = 30;
+    i3.step = 1;
+
+    inputsArray.push(i1);
+    inputsArray.push(i2);
+    inputsArray.push(i3);
+
+    return inputsArray;
+  }
 
 
 
@@ -32,9 +69,11 @@ angular.module('subModule', [])
 
 
   return {
-    currencies: currencies,
-    convert: convert,
-    calcAnnualIncome: calcAnnualIncome
+    // currencies: currencies,
+    // convert: convert,
+    calcAnnualIncome: calcAnnualIncome,
+    initializeInputsDisplays: this.initializeInputsDisplays
+
   };
 
 });
