36be02637c97df987da2eb33da53079cbe603566
KletusVandamme <matt.hill.email@gmail.com>
2015-04-15 01:47:07 -0700

    refactor

--- app/js/subModule.js
+++ app/js/subModule.js
@@ -54,7 +54,6 @@ angular.module('subModule', [])
     i1.name = 'projectCost';
     i1.displayName = 'Project Cost ($)';
     i1.value = 1400000;
-    i1.valueString = 'xxx';
     i1.min = 0;
     i1.max = 2000000;
     i1.step = 100;
@@ -63,7 +62,6 @@ angular.module('subModule', [])
     i2.name = 'ownerEquity';
     i2.displayName = 'Owner Equity (%)';
     i2.value = 25;
-    i2.valueString = 'zzz';
     i2.min = 0;
     i2.max = 100;
     i2.step = 1;
@@ -72,7 +70,6 @@ angular.module('subModule', [])
     i3.name = 'interestRate';
     i3.displayName = 'Interest Rate (WACC) (%)';
     i3.value = 12;
-    i3.valueString = 'xxx';
     i3.min = 0;
     i3.max = 30;
     i3.step = 1;
