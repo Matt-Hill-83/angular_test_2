0aeacaf894eca9d6b498d3d7b7506e0c80f25c0a
KletusVandamme <matt.hill.email@gmail.com>
2015-04-15 01:08:23 -0700

    cleaned out old gui elements

--- app/js/app.js
+++ app/js/app.js
@@ -1,8 +1,25 @@
+// Monkey patch in new function on String.prototype to format currency numbers
+String.prototype.insertComma = function() {
+  // Remove number past decimal.
+  var arr  = this.split(".");
+  var newStr = arr[0];
+
+  // Insert commas at appropriate locations.
+  if (newStr.length > 6) {
+    return (newStr.slice(0,-6) + "," + newStr.slice(-6, -3) + "," + newStr.slice(-3));
+  } else if (newStr.length > 3) {
+    return (newStr.slice(0,-3) + "," + newStr.slice(-3));
+  }else{
+    return newStr.slice(0);
+  }
+};
+
+
+
 'use strict';
 
 angular.module('myApp', ['testService', 'subModule'])
 .controller('solarCtrl', ['solarCalculator', function(solarCalculator) {
-  this.results = monthlyResults;
 
   this.inputsArray = solarCalculator.initInputsDisplays();
   this.initSubExpensesDisplays = solarCalculator.initSubExpensesDisplays();
